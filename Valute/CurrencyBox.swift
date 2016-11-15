3b7881882fd7f13c00cbbcf8cd78f4746fd36e19
Milovan Minic <milovan.minic@gmail.com>
2016-11-15 17:53:01 +0100

    Added new CocoaTouch class, called CurrencyBox subclassed UIView and assigned that in IB (InterfaceBuilder)

---
+++ Valute/CurrencyBox.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyBox.swift
+//  Valute
+//
+//  Created by iosakademija on 11/15/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyBox: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
