fb0e54e86103409914cceb48a601e42be72dac9f
Milovan Minic <milovan.minic@gmail.com>
2016-11-19 13:47:05 +0100

    Added function body that finds country code based on currency code

--- Valute/Locale-Extension.swift
+++ Valute/Locale-Extension.swift
@@ -14,6 +14,27 @@ extension Locale {
     
     static func countryCode(forCurrencyCode currencyCode: String) -> String? {
         
+        for countryCode in NSLocale.isoCountryCodes {
+        
+            //	we need to build LocaleIdentifier, which can have many components
+            //	one way to do it is to start from the components we do have
+            //	in this case countryCode
+            
+            let comps = [NSLocale.Key.countryCode.rawValue: countryCode]
+            
+            //	now use the components to instantiate the identifier
+            let localeIdentifier = identifier(fromComponents: comps)
+            //	and then use it to build specific Locale instance for that id
+            let locale = Locale(identifier: localeIdentifier)
+            
+            //	now ask this Locale instance for any other info you need
+            guard let cc = locale.currencyCode else { continue }
+            
+            if cc == countryCode {
+                return countryCode
+            }
+        }
+        
         return nil
         
     }
