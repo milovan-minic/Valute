9c1434da8d9d5ab7ca255ab3838a492348481b95
Milovan Minic <milovan.minic@gmail.com>
2016-11-19 13:31:32 +0100

    Added declaration of static method that returns country code based on currency code. This extends Locale framework.

---
+++ Valute/Locale-Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Locale-Extension.swift
+//  Valute
+//
+//  Created by iosakademija on 11/19/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import Foundation
+
+extension Locale {
+    //	since our flag icons are all named using country codes
+    //	we need to match the currency codes to the country codes
+    
+    static func countryCode(forCurrencyCode currencyCode: String) -> String? {
+        
+        return nil
+        
+    }
+}
