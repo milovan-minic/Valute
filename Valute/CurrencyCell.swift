623b706bef289887fe3d24beb5ea989c75612684
Milovan Minic <milovan.minic@gmail.com>
2016-11-20 15:18:07 +0100

    Added CurrencyCell subclassing UITableViewCell and set the prototype cell's class to CurrencyCell

---
+++ Valute/CurrencyCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyCell.swift
+//  Valute
+//
+//  Created by iosakademija on 11/20/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
